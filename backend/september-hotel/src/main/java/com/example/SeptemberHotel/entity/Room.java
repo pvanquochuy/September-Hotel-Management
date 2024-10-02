2ace52c2ed413469ed2707f31a460a97a1c8c508
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-02 22:54:05 +0700

    create entities

---
+++ backend/september-hotel/src/main/java/com/example/SeptemberHotel/entity/Room.java
@@ -0,0 +1,38 @@
+package com.example.SeptemberHotel.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookingConfirmationCode;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\''+
+                '}';
+    }
+}
