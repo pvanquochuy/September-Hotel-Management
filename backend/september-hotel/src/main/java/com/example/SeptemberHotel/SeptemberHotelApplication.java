e6085bda1067ddd09be1777d709f732143d79442
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-02 22:00:32 +0700

    Initial commit

---
+++ backend/september-hotel/src/main/java/com/example/SeptemberHotel/SeptemberHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.SeptemberHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptemberHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeptemberHotelApplication.class, args);
+	}
+
+}
