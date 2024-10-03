5e81c1fb17ab89d07e6c95c0a6ff26d3ba0ea1a2
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-03 16:25:07 +0700

    Security filter chain

---
+++ backend/september-hotel/src/main/java/com/example/SeptemberHotel/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.SeptemberHotel.security;
+
+import com.example.SeptemberHotel.service.CustomeUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity // Kích hoạt bảo mật trên các phương thức (method-level security), cho phép sử dụng các annotation như @PreAuthorize, @Secured, v.v.
+@EnableWebSecurity // Kích hoạt tính năng bảo mật web của Spring Security.
+public class SecurityConfig {
+
+    @Autowired
+    private CustomeUserDetailService customeUserDetailService;
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults()) //Áp dụng cấu hình CORS
+                .authorizeHttpRequests(request -> request //  Định nghĩa quy tắc phân quyền:
+                        .requestMatchers("/auth/**", "rooms/**", "/bookings/**").permitAll() //  Cho phép truy cập không cần xác thực vào các endpoint
+                        .anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    // lấy thông tin người dùng và PasswordEncoder để mã hóa mật khẩu.
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customeUserDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    //  Một PasswordEncoder sử dụng thuật toán BCrypt để mã hóa mật khẩu
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Quản lý các AuthenticationProvider và xử lý quá trình xác thực người dùng.
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
