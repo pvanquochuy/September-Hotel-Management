f7c14ef7fbdd8756dc55974b7751465c5806d9ae
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-03 15:33:50 +0700

    Cors config

---
+++ backend/september-hotel/src/main/java/com/example/SeptemberHotel/sercurity/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.SeptemberHotel.sercurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @pvanquochuy
+ * cấu hình CORS
+ */
+@Configuration
+public class CorsConfig {
+    @Bean //  Đánh dấu phương thức này để Spring tạo và quản lý bean WebMvcConfigurer.
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") //  Áp dụng quy tắc CORS cho tất cả các endpoint trong ứng dụn
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Chỉ định các phương thức HTTP được phép từ các nguồn gốc khác.
+                        .allowedOrigins("*");  //  Chỉ định các nguồn gốc (origins) được phép truy cập tài nguyên từ ứng dụng.
+            }
+        };
+    }
+}
