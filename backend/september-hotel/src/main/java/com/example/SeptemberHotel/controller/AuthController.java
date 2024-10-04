40948efda1daa535bd65c159a4b1df26ecd30e83
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-04 11:25:44 +0700

    auth controller

---
+++ backend/september-hotel/src/main/java/com/example/SeptemberHotel/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.SeptemberHotel.controller;
+
+import com.example.SeptemberHotel.dto.LoginRequest;
+import com.example.SeptemberHotel.dto.Response;
+import com.example.SeptemberHotel.entity.User;
+import com.example.SeptemberHotel.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user){
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest){
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
